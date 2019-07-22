28f7f21a38fc7ba48fe33c856cb36c07685e3953
GBTC441007ur <samduru1@gmail.com>
2019-07-22 16:57:40 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long employeeID;
+
+    private String employeeName;
+    private String employeeRole;
+
+    @ManyToOne()
+    private Department department;
+
+    public long getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(long employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeRole() {
+        return employeeRole;
+    }
+
+    public void setEmployeeRole(String employeeRole) {
+        this.employeeRole = employeeRole;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Employee() {
+    }
+}
